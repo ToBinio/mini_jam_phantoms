b5b07a000ab699415baf0a1c316820ec721c9647
Eren <erengulum16@gmail.com>
2026-07-17 13:27:07 +0200

    simple possession implemented (only visually)

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,19 +5,45 @@ extends CharacterBody2D
 @export var friction = 800.0
 @export var acceleration = 400.0
 
+@onready var shape_cast_2d: ShapeCast2D = $ShapeCast2D
 
 func _physics_process(delta: float) -> void:
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		possess_nearby_sprite()
+	
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if direction != Vector2.ZERO:
 		velocity = velocity.move_toward(direction*speed, acceleration * delta)
 		
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
-
+	
+	if velocity.x > 0:
+		$Sprite2D.flip_h = false
+	elif velocity.x < 0:
+		$Sprite2D.flip_h = true
+	
 	move_and_slide()
+
+
+func possess_nearby_sprite():
+	shape_cast_2d.force_shapecast_update()
+	
+	for i in shape_cast_2d.get_collision_count():
+		var body = shape_cast_2d.get_collider(i)
+		
+		if body == self:
+			continue
+		
+		var other_sprite = body.get_node("Sprite2D")
+		var my_sprite = $Sprite2D
+		
+		my_sprite.texture = other_sprite.texture
+		my_sprite.flip_h = other_sprite.flip_h
+		
+		body.queue_free()
+		
+		global_position = body.global_position
+		
+		break
