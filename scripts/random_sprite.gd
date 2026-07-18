bbd6dd5f0cbaafd1a6fd7ba83a19584a4ec69b6c
tobinio <tobias.frischmann@proton.me>
2026-07-18 15:53:44 +0200

    add coral

--- scripts/random_sprite.gd
+++ scripts/random_sprite.gd
@@ -1,7 +1,11 @@
 extends Sprite2D
 
 @export var number_of_sprites: int;
+@export var flip_sprites: bool = false
 
 func _ready() -> void:
 	var atlas = texture as AtlasTexture
 	atlas.region.position.x = 16 * randi_range(0, number_of_sprites - 1);
+	
+	if flip_sprites:
+		flip_h = randf() > 0.5
