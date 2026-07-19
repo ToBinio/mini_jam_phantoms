344242b7a3320877c6b631ed731315746f2b3a74
Eren <erengulum16@gmail.com>
2026-07-19 16:58:39 +0200

    further level design update

--- scripts/blocker.gd
+++ scripts/blocker.gd
@@ -14,7 +14,7 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body is not Player:
 		return
 	
-	if body.global_position.x > global_position.x:
+	if body.global_position.x < global_position.x:
 		body.set_light(true)
 	else:
 		body.set_light(false)
